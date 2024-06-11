f7ed1d915d605c7872dab5def9a656a1ec6f2221
NoMeLlamoDante <eo.zm24@outlook.com>
2024-06-10 18:38:01 -0600

    ejemplo de navbar

--- link_bio/link_bio/link_bio.py
+++ link_bio/link_bio/link_bio.py
@@ -5,7 +5,16 @@ class State(rx.State):
 
 
 def index() -> rx.Component:
-    return rx.text("Hola reflex", color="blue")
+    return rx.box(
+        rx.hstack(
+            rx.heading("Dante", size="7", weight="bold"),
+            rx.heading("NoMeLlamoDante", size="3",weight="light", padding=".5rem"),
+            justify="between",
+            position="sticky",
+            background="grey",
+            padding="1rem",
+        ),
+    )
 
 app = rx.App()
 app.add_page(index)
