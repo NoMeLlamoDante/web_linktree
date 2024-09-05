31853e22050c4c5a0f4dedf594ee80f120254ee2
NoMeLlamoDante <eo.zm24@outlook.com>
2024-09-05 12:57:08 -0600

    creamos iconos con boton

--- link_bio/link_bio/componentes/link_button.py
+++ link_bio/link_bio/componentes/link_button.py
@@ -19,3 +19,17 @@ def link_button(label: str, url: str, icon:str = None) -> rx.Component:
         is_external=True,
         width="100%",
     )
+    
+def icon_button(url, str, icon:str = None) -> rx.Component:
+    return rx.link(
+        rx.button(
+            rx.hstack(
+                rx.icon(icon if icon else "external-link"),
+                widht="100%",
+                align="center",
+            ),
+        ),
+        href=url,
+        is_external=True,
+        width="100%",
+    )
