a55b52c00558e712c6d6e64ad1e87d52d68716f5
NoMeLlamoDante <eo.zm24@outlook.com>
2025-02-07 20:30:10 -0600

    docstrings links

--- link_bio/link_bio/views/links.py
+++ link_bio/link_bio/views/links.py
@@ -1,21 +1,16 @@
+""" Web component: Links section"""
 import reflex as rx
 from link_bio.componentes.link_button import link_button
 from link_bio.componentes.title import title
 from link_bio.componentes import constants
 
+
 def links() -> rx.Component:
-    return  rx.vstack(
+    """ Reflex component: links section"""
+    return rx.vstack(
         title("Links"),
-        link_button(
-            "Github",
-            constants.GITHUB_URL, 
-            "github"
-        ),
-        link_button(
-            "linkedin",
-            constants.LINKEDIN_URL,
-            "linkedin"
-        ),
+        link_button("Github", constants.GITHUB_URL, "github"),
+        link_button("linkedin", constants.LINKEDIN_URL, "linkedin"),
         # link_button(
         #     "pokeword search",
         #     constants.POKESEARCH_URL,
