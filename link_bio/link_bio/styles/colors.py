52e5868336daf4c4b7da16b8ac5ab3471d8a9949
NoMeLlamoDante <eo.zm24@outlook.com>
2025-02-07 18:01:15 -0600

    refactoring colors

--- link_bio/link_bio/styles/colors.py
+++ link_bio/link_bio/styles/colors.py
@@ -1,10 +1,12 @@
+""" Color Styles """
 from enum import Enum
 
+
 class Color(Enum):
+    """ page colors """
     BLACK = "#161b1c"
     GREEN = "#004638"
     LIGHT_GREEN = "#6dedb2"
     WHITE = "#fff9e4"
     LIGHT_GREY = "#787878" 
     GREY = "#3b3338"
-    
