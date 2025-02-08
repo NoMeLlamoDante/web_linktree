910b6f267cded46959a51ddd910293e0189e9ea3
NoMeLlamoDante <eo.zm24@outlook.com>
2025-02-07 18:25:56 -0600

    refactoring header

--- link_bio/link_bio/views/header/header.py
+++ link_bio/link_bio/views/header/header.py
@@ -1,22 +1,24 @@
+""" Web section: Header """
 import reflex as rx
 from link_bio.componentes.title import principal
-from link_bio.styles.styles import Size, Color
-import link_bio.componentes.text as text
+from link_bio.styles.styles import Size
+from link_bio.componentes import text
 from link_bio.componentes.link_button import icon_button
-from  link_bio.componentes import constants
+from link_bio.componentes import constants
 
 
 def header() -> rx.Component:
+    """ Reflex module: Header """
     return rx.section(
         rx.flex(
             rx.stack(
                 rx.stack(
                     rx.avatar(
                         src="SB3Ail01.svg",
-                        variant="solid", 
-                        high_contrast=False, 
+                        variant="solid",
+                        high_contrast=False,
                         radius="large",
-                        fallback="Dante", 
+                        fallback="Dante",
                         size="7",
                         z_index="-1",
                         alt="Logotipo de Edgar Zarate"
@@ -26,28 +28,20 @@ def header() -> rx.Component:
                     justify="center",
                 ),
                 rx.vstack(
-                    principal(text.name),
-                    rx.text(text.sub_name_thinking, 
-                        size="4",
-                    ),
-                    #Ubicación
+                    principal(text.NAME),
+                    rx.text(text.JOB, size="4"),
+                    # Ubicación
                     rx.card(
                         rx.hstack(
                                 rx.icon("map-pin"),
-                                rx.text(text.city,weight="light"),
+                                rx.text(text.CITY, weight="light"),
                         ),
                         variant="ghost"
                     ),
-                    #links
+                    # links
                     rx.hstack(
-                        icon_button(
-                            constants.GITHUB_URL, 
-                            "github"
-                        ),
-                        icon_button(
-                            constants.LINKEDIN_URL,
-                            "linkedin"
-                        ),
+                        icon_button(constants.GITHUB_URL, "github"),
+                        icon_button(constants.LINKEDIN_URL, "linkedin"),
                     ),
                     flex_direction="column",
                     width="100%",
@@ -63,9 +57,11 @@ def header() -> rx.Component:
         ),
         rx.vstack(
             rx.text(
-                text.complete_bio,
+                text.COMPLETE_BIO,
                 font_size="3",
-                padding_y=rx.breakpoints(initial=Size.NORMAL.value, sm=Size.NORMAL_LARGE.value),
+                padding_y=rx.breakpoints(
+                    initial=Size.NORMAL.value,
+                    sm=Size.NORMAL_LARGE.value),
             ),
         ),
         width=rx.breakpoints(initial="90%", sm="60%"),
